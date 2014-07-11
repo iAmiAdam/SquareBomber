955235f0067dd0654336b974d268d3ba8847381e
Adam Smith <adam.smith18@ntlworld.com>
2014-07-11 20:24:26 +0100

    Game Object Class and package

---
+++ core/src/info/adamjsmith/squarebomber/objects/GameObject.java
@@ -0,0 +1,10 @@
+package info.adamjsmith.squarebomber.objects;
+
+public class GameObject extends Object {
+	public static final int IDPlayer = 0;
+	public static final int IDCrate = 1;
+	public static final int IDBomb = 2;
+	public static final int IDExplosion = 3;
+		
+	public int ID;
+}
