b736d88b090621165cbeca6377f69d6f3a644404
Adam Smith <adam.smith18@ntlworld.com>
2014-07-11 15:04:04 +0100

    Map rendering

--- core/src/info/adamjsmith/squarebomber/SquareBomber.java
+++ core/src/info/adamjsmith/squarebomber/SquareBomber.java
@@ -1,27 +1,33 @@
 package info.adamjsmith.squarebomber;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import info.adamjsmith.squarebomber.screens.GameScreen;
 
-public class SquareBomber extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+
+public class SquareBomber extends Game {
+	SquareBomber game;
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new GameScreen());
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();	
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+	}
+	
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+	
+	public void pause() {
+		super.pause();
 	}
 }
