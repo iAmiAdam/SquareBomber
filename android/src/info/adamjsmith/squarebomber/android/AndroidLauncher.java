e10334f3f81f249c0f69806a592a1e650336b72a
Adam Smith <adam.smith18@ntlworld.com>
2014-07-11 13:48:18 +0100

    Initial Commit

---
+++ android/src/info/adamjsmith/squarebomber/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package info.adamjsmith.squarebomber.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import info.adamjsmith.squarebomber.SquareBomber;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SquareBomber(), config);
+	}
+}
