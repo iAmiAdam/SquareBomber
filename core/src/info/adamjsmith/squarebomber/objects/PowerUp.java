2bbf9433d754c62c3adc357b15538e114f4907f7
Adam Smith <adam.smith18@ntlworld.com>
2014-07-14 17:25:37 +0100

    Power Up class finished

--- core/src/info/adamjsmith/squarebomber/objects/PowerUp.java
+++ core/src/info/adamjsmith/squarebomber/objects/PowerUp.java
@@ -7,8 +7,21 @@ public class PowerUp extends StaticObject{
 	public int ID = GameObject.IDPowerUp;
 	public int type;
 	
-	public PowerUp() {
+	public PowerUp(float x, float y) {
 		Random rand = new Random();
-		type = rand.nextInt((3 - 1) + 1 ) + 1;
+		this.type = rand.nextInt((3 - 1) + 1 ) + 1;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void givePower(Player player) {
+		switch (this.type) {
+		case 1:
+			player.bombs++;
+		case 2:
+			player.power++;
+		case 3:
+			player.speed += 0.5;
+		}
 	}
 }
