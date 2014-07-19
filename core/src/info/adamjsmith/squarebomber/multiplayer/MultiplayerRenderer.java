61c461fb75c5da65f357d94599ac09cd6a5c496d
Adam Smith <adam.smith18@ntlworld.com>
2014-07-19 10:42:18 +0100

    Restarting multiplayer aspect

--- core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerRenderer.java
+++ core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerRenderer.java
@@ -1,47 +1,55 @@
 package info.adamjsmith.squarebomber.multiplayer;
 
-import info.adamjsmith.squarebomber.GameRenderer;
 import info.adamjsmith.squarebomber.SquareBomber;
 import info.adamjsmith.squarebomber.objects.Player;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
 
-public class MultiplayerRenderer extends GameRenderer {
+public class MultiplayerRenderer {
 	
 	private MultiplayerUpdater world;
 	private SquareBomber game;
 	private Player player;
 	
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
+	private Matrix4 uiMatrix;
+	
+	private float stateTime;
+	private TextureRegion currentFrame;
+	
 	public MultiplayerRenderer(SquareBomber game, MultiplayerUpdater world, String myID) {
-		super(game, world);
 		this.world = world;
 		this.game = game;
 		this.player = world.getCurrentPlayer(myID);
+		this.batch = new SpriteBatch();
+		renderer = new OrthogonalTiledMapRenderer(game.assets.map, 1/128f);
+		camera = new OrthographicCamera(19f, 19f);
+		camera.viewportWidth = 7f;
+		camera.viewportHeight = 5f;
+		
+		uiMatrix = camera.combined.cpy();
+		uiMatrix.setToOrtho2D(0, 0, 7f, 5f);
 	}
 	
-	@Override 
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.position.set(player.getX(), player.getY(), 0);
+		camera.position.set(2.5f, 2.5f, 0);
 		camera.update();
 		
 		renderer.setView(camera);
 		renderer.render();
+		Gdx.app.log("In The", "Renderer");
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		drawCrates();
-		drawPowerUps();
-		drawPlayers();
-		drawBombs();
-		drawExplosions();
-		batch.setProjectionMatrix(uiMatrix);
-		batch.draw(game.assets.controller, 6f, 0f, 1f, 1f);
-		batch.end();
 		
-		stateTime += Gdx.graphics.getDeltaTime();
 	}
 	
 	private void drawPlayers() {
