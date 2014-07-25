dde63de7d7529e95d720a9ea1d30f84c7364736b
Adam Smith <adam.smith18@ntlworld.com>
2014-07-25 08:52:13 +0100

    Current player appearing in multiplayer

--- core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerRenderer.java
+++ core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerRenderer.java
@@ -1,8 +1,6 @@
 package info.adamjsmith.squarebomber.multiplayer;
 
-import info.adamjsmith.squarebomber.GameRenderer;
 import info.adamjsmith.squarebomber.SquareBomber;
-import info.adamjsmith.squarebomber.objects.Player;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,25 +10,34 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Matrix4;
 
-public class MultiplayerRenderer extends GameRenderer {
+public class MultiplayerRenderer {
 	
 	private MultiplayerUpdater world;
 	private SquareBomber game;
 	//private Player player;
 	
-	//private SpriteBatch batch;
-	//private OrthographicCamera camera;
-	//private OrthogonalTiledMapRenderer renderer;
-	//private Matrix4 uiMatrix;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
+	private Matrix4 uiMatrix;
 	
-	private float stateTime;
+	private float stateTime = 0;
 	private TextureRegion currentFrame;
 	
 	public MultiplayerRenderer(SquareBomber game, MultiplayerUpdater world) {
-		super(game, world);
+		this.game = game;
+		this.world = world;
+		
+		batch = new SpriteBatch();
+		renderer = new OrthogonalTiledMapRenderer(game.assets.map, 1/128f);
+		camera = new OrthographicCamera(19f, 19f);
+		camera.viewportWidth = 7f;
+		camera.viewportHeight = 5f;
+		
+		uiMatrix = camera.combined.cpy();
+		uiMatrix.setToOrtho2D(0, 0, 7f, 5f);
 	}
 	
-	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -45,5 +52,26 @@ public class MultiplayerRenderer extends GameRenderer {
 		batch.setProjectionMatrix(uiMatrix);
 		batch.draw(game.assets.controller, 6f, 0f, 1f, 1f);
 		batch.end();
+		
+		stateTime += Gdx.graphics.getDeltaTime();
+	}
+	
+	private void drawPlayer() {
+		switch(world.player.direction) {
+		case STOP:
+			batch.draw(game.assets.playerStop, world.player.getX() - (world.player.getWidth() / 2), world.player.getY() - (world.player.getHeight() / 2), 
+					world.player.getWidth() / 2, world.player.getHeight() / 2,
+					world.player.getWidth(), world.player.getHeight(),
+					1f, 1f, world.player.rotation);
+			stateTime = 0;
+			break;
+		default:
+			currentFrame = game.assets.playerWalk.getKeyFrame(stateTime, true);
+			batch.draw(currentFrame, world.player.getX() - (world.player.getWidth() / 2), world.player.getY() - (world.player.getHeight() / 2), 
+					world.player.getWidth() / 2, world.player.getHeight() / 2,
+					world.player.getWidth(), world.player.getHeight(),
+					1f, 1f, world.player.rotation);
+			break;
+		}
 	}
 }
--- core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerUpdater.java
+++ core/src/info/adamjsmith/squarebomber/multiplayer/MultiplayerUpdater.java
@@ -1,23 +1,24 @@
 package info.adamjsmith.squarebomber.multiplayer;
 
-import info.adamjsmith.squarebomber.GameUpdater;
 import info.adamjsmith.squarebomber.SquareBomber;
 import info.adamjsmith.squarebomber.objects.Player;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.nextpeer.android.NextpeerTournamentStartData;
 
-public class MultiplayerUpdater extends GameUpdater {
+public class MultiplayerUpdater {
 	
+	World world;
 	public Player[] players;
 	public Player player;
 	
 	public MultiplayerUpdater(SquareBomber game, NextpeerTournamentStartData startData) {
-		super(game);
+		world = new World(new Vector2(0, 0), false);
 		player = new Player(world, 2.5f, 2.5f);
 		player.playerId = startData.currentPlayer.playerId;
 	}
 	
-	@Override
 	public void update() {
 		world.step(1/45f, 4, 6);
 	}	
